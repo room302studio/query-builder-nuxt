f73a33e0570c0399f08da70c32935f4d5d59f2df
EJ Fox <ejfox@ejfox.com>
2023-11-22 11:05:48 -0500

    style: style the active conditions view a bit

--- pages/index.vue
+++ pages/index.vue
@@ -1,80 +1,68 @@
 <template>
   <section class="p-5">
     <section>
-      <h2>Query builder</h2>
+      <h2 class="text-2xl font-bold">Query builder</h2>
 
-      <h3>Columns</h3>
-      <ul>
-        <li v-for="col in columns" :key="col.key">
+      <h3 class="text-lg font-semibold mt-5">Columns</h3>
+      <ul class="flex flex-wrap mt-2">
+        <li v-for="col in columns" :key="col.key"
+          class="inline-block mr-2 mb-2 font-light bg-gray-200 text-black px-3 py-1 rounded">
           {{ col.label }}
         </li>
       </ul>
-
-      <!-- based on /**
- * Add a condition to the query builder
- * @param {string} columnName - the name of the column
- * @param {string} type - the type of condition
- * @param {string} value - the value to compare
- */
-function addCondition(columnName, type, value) {
-  conditions.value.push({ columnName, type, value })
-}
-
-function removeCondition(index) {
-  conditions.value.splice(index, 1)
-}
-
-function modifyCondition(index, columnName, type, value) {
-  conditions.value[index] = { columnName, type, value }
-}
-
-build the interface for a query builder that allows adding unlimited stacked conditions -->
       <section class="p-5">
-        <h3>Conditions</h3>
+        <h3 class="text-lg font-semibold">Conditions</h3>
         <section class="p-5">
-          <h4>Add condition</h4>
+          <h4 class="text-md font-semibold">Add condition</h4>
           <section class="p-5">
-            <label for="column">Column</label>
-            <select name="column" id="column" v-model="conditionColumn">
-              <option value="">Select a column</option>
-              <option v-for="col in columns" :key="col.key" :value="col.key">
-                {{ col.label }}
-              </option>
-            </select>
+            <label for="column" class="text-sm font-medium">Column</label>
+            <USelect v-model="conditionColumn" :options="columns" class="mt-1" />
           </section>
           <section class="p-5" v-if="conditionColumn">
-            <label for="type">Type</label>
-            <select name="type" id="type" v-model="conditionType">
-              <option value="">Select a type</option>
-              <option value="equals">Equals</option>
-              <option value="not-equals">Not equals</option>
-              <option value="contains">Contains</option>
-              <option value="not-contains">Not contains</option>
-              <!-- <option value="greater-than">Greater than</option>
-              <option value="less-than">Less than</option> -->
-            </select>
+            <label for="type" class="text-sm font-medium">Type</label>
+            <USelect v-model="conditionType" class="mt-1" :options="[
+              { value: '', label: 'Select a type' },
+              { value: 'equals', label: 'Equals' },
+              { value: 'not-equals', label: 'Not equals' },
+              { value: 'contains', label: 'Contains' },
+              { value: 'not-contains', label: 'Not contains' }
+            ]" />
           </section>
           <section class="p-5" v-if="conditionType">
-            <label for="value">Value</label>
-            <!-- <input type="text" name="value" id="value" /> -->
-            <select name="value" id="value" v-model="conditionValue">
-              <option value="">Select a value</option>
-              <option v-for="value in columnUniqueValues[conditionColumn]" :key="value" :value="value">
-                {{ value }}
-              </option>
-            </select>
+            <label for="value" class="text-sm font-medium">Value</label>
+            <USelect v-model="conditionValue" class="mt-1" :options="columnUniqueValues[conditionColumn]">
+              <template #default="{ option }">
+                <option :value="option">{{ option }}</option>
+              </template>
+            </USelect>
           </section>
           <section class="p-5" v-if="conditionColumn && conditionType">
-            <button @click="addCondition(conditionColumn, conditionType, conditionValue)">Add condition</button>
+            <UButton @click="addCondition(conditionColumn, conditionType, conditionValue)"
+              class="bg-blue-500 text-white px-4 py-2 rounded">Add condition</UButton>
           </section>
 
           <!-- list all the conditions and let the user modify or delete them -->
           <section class="p-5 border border-gray-500" v-if="conditions.length > 0">
-            <h4>Active Conditions</h4>
+            <h4 class="text-md font-semibold mb-4">Active Conditions</h4>
             <ul>
-              <li v-for="(condition, index) in conditions" :key="index">
-                {{ condition.columnName }} {{ condition.type }} {{ condition.value }}
-                <button @click="removeCondition(index)">Remove</button>
+              <li v-for="(condition, index) in conditions" :key="index" class="flex items-center justify-between my-8">
+                <div class="flex flex-col w-3/12">
+                  <span class="font-semibold break-words break">{{ condition.columnName }}</span>
+                  <span :class="{
+                    'text-sm text-gray-500': true,
+                    'text-green-500': condition.type === 'equals',
+                    'text-red-500': condition.type === 'not-equals'
+                  }">
+                    {{ condition.type }}
+                  </span>
+                </div>
+                <span class="flex-grow px-4">
+                  <span class="font-semibold p-4 border border-gray-500 rounded text-sm">
+                    {{ condition.value }}
+                  </span>
+                </span>
+                <UButton @click="removeCondition(index)" color="red">Remove
+                </UButton>
               </li>
             </ul>
           </section>
